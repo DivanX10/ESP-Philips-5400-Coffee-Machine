d6e5489da776e932eef921cbc0b503b593a9fc60
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2023-06-20 16:16:12 +0300

    Update __init__.py

--- components/philips_series_5400/__init__.py
+++ components/philips_series_5400/__init__.py
@@ -1,34 +1,29 @@
-from typing_extensions import Required
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
-from esphome.components.uart import UARTComponent
-
+from esphome.components import uart
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['uart']
 
+philips_series_5400_ns = cg.esphome_ns.namespace('philips_series_5400')
+
+PhilipsSeries5400 = philips_series_5400_ns.class_('PhilipsSeries5400', cg.Component)
+
 DISPLAY_UART_ID = 'display_uart'
 MAINBOARD_UART_ID = 'mainboard_uart'
-CONTROLLER_ID = 'controller_id'
 
-philips_series_5400_ns = cg.esphome_ns.namespace('philips_series_5400')
-PhilipsSeries5400 = philips_series_5400_ns.class_(
-    'PhilipsSeries5400', cg.Component)
-
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(PhilipsSeries5400),
-    cv.Required(DISPLAY_UART_ID): cv.use_id(UARTComponent),
-    cv.Required(MAINBOARD_UART_ID): cv.use_id(UARTComponent),
-}).extend(cv.COMPONENT_SCHEMA)
+    cv.Required(DISPLAY_UART_ID): cv.use_id(uart.UARTComponent),
+    cv.Required(MAINBOARD_UART_ID): cv.use_id(uart.UARTComponent),
+})
 
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
 
-    display = yield cg.get_variable(config[DISPLAY_UART_ID])
-    mainboard = yield cg.get_variable(config[MAINBOARD_UART_ID])
+    display = await cg.get_variable(config[DISPLAY_UART_ID])
     cg.add(var.register_display_uart(display))
+    mainboard = await cg.get_variable(config[MAINBOARD_UART_ID])
     cg.add(var.register_mainboard_uart(mainboard))
-
